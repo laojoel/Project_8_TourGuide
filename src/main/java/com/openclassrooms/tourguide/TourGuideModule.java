b048410cdf4a4855448d05437686543d7ff0996f
Joel LAO <joel.lao@axa.fr>
2025-02-19 10:45:51 +0100

    init alloc

---
+++ src/main/java/com/openclassrooms/tourguide/TourGuideModule.java
@@ -0,0 +1,27 @@
+package com.openclassrooms.tourguide;
+
+import com.openclassrooms.tourguide.service.RewardsService;
+import gpsUtil.GpsUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import rewardCentral.RewardCentral;
+
+@Configuration
+public class TourGuideModule {
+	
+	@Bean
+	public GpsUtil getGpsUtil() {
+		return new GpsUtil();
+	}
+	
+	@Bean
+	public RewardsService getRewardsService() {
+		return new RewardsService(getGpsUtil(), getRewardCentral());
+	}
+	
+	@Bean
+	public RewardCentral getRewardCentral() {
+		return new RewardCentral();
+	}
+	
+}
